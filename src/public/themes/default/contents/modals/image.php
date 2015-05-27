f2a97ede3ba631f0e3df96847556d6489df2a93c
Nicholas Jordon <projectcleverweb@gmail.com>
2015-05-27 17:36:11 -0400

    theme: added 'modals' content; Theme: added 'image' content/modal

---
+++ src/public/themes/default/contents/modals/image.php
@@ -0,0 +1,10 @@
+<div class="ui small modal" id="modal-image-preview">
+	<i class="close icon"></i>
+	<div class="ui header">
+		filename.png
+		<div class="sub header">/path/to/filename.png</div>
+	</div>
+	<div class="ui center aligned basic content segment">
+		<a href="#"><img src="http://placehold.it/3000x3000"></a>
+	</div>
+</div>
